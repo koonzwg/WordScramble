a0fdf263f6faf78ee1f15dcc74c1fb724fd2da75
koonzwg <koonzwg@gmail.com>
2024-03-17 17:37:16 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by William Koonz on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
